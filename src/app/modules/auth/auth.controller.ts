68cdf0ee6c2c5a4eafd038bd487485d0781aeaa4
Md Shimul Molla <shimuliithost@gmail.com>
2025-09-08 17:55:45 +0600

    Set cookies credensials

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -62,13 +62,13 @@ const logout = catchAsync(async (req: Request, res: Response, next: NextFunction
 
     res.clearCookie("accessToken", {
         httpOnly: true,
-        secure: false,
-        sameSite: "lax"
+        secure: true,
+        sameSite: "none"
     })
     res.clearCookie("refreshToken", {
         httpOnly: true,
-        secure: false,
-        sameSite: "lax"
+        secure: true,
+        sameSite: "none"
     })
 
     sendResponse(res, {
